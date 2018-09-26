0c397b11f1ecf66e8c893dd1e0d60c6c6cd29b62
Liu Xia <lxconan@gmail.com>
2018-09-26 18:49:16 +0800

    [liuxia] Initial commit.

---
+++ src/main/java/com/tw/relife/RelifeApp.java
@@ -0,0 +1,18 @@
+package com.tw.relife;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class RelifeApp implements RelifeAppHandler {
+    private final RelifeAppHandler handler;
+
+    public RelifeApp(RelifeAppHandler handler) {
+        // TODO: You can start here
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public RelifeResponse process(RelifeRequest request) {
+        // TODO: You can start here
+        throw new NotImplementedException();
+    }
+}
